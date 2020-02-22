ad64347f612fefe44670e257d4d6fdc55012985d
SuperNG6 <sleele@gmail.com>
2020-02-22 11:41:48 +0800

    Update delete.aria2.sh

--- root/aria2/script/delete.aria2.sh
+++ root/aria2/script/delete.aria2.sh
@@ -5,6 +5,7 @@ downloadpath='/downloads'
 filepath=$3
 rdp=${filepath#${downloadpath}/}
 path=${downloadpath}/${rdp%%/*}
+dirpath=${$filepath%/* }
 
 echo "Called with num[$2] filepath[$3]" >> /config/log.txt
 
@@ -17,7 +18,7 @@ elif [ "$path" = "$filepath" ] && [ $2 -eq 1 ]
         exit 0
 elif [ "$path" != "$filepath" ] && [ $2 -gt 1 ]
     then
-        [ -e "$path".aria2 ] && rm -vf "$path".aria2
+        [ -e "$path".aria2 ] && rm -vf "$dirpath".aria2
         exit 0
 elif [ "$path" != "$filepath" ] && [ $2 -eq 1 ]
     then
