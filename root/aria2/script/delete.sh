37eba57d44b3cdf5c83cfa8610fa5a868df9c99d
SuperNG6 <sleele@gmail.com>
2020-02-22 20:42:58 +0800

    Update delete.sh

--- root/aria2/script/delete.sh
+++ root/aria2/script/delete.sh
@@ -7,6 +7,7 @@ rdp=${filepath#${downloadpath}/}
 path=${downloadpath}/${rdp%%/*}
 dirpath=${filepath%/*}
 
+
 if [ $2 -eq 0 ]
     then
         exit 0
@@ -20,7 +21,6 @@ elif [ "$path" != "$filepath" ] && [ $2 -gt 1 ]
         exit 0
 elif [ "$path" != "$filepath" ] && [ $2 -eq 1 ]
     then
-        [ -e "$filepath".aria2 ] && rm -vf "$filepath".aria2 "$filepath"
-        find "${downloadpath}" ! -path "${downloadpath}" -depth -type d -empty -exec rm -vrf {} \;
+        [ -e "$dirpath".aria2 ] && rm -vf "$dirpath".aria2 "$dirpath"
         exit 0
 fi
