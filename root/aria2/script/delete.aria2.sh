a06f64bae2e0ea8b4a74b50fb1543238eb0bdefe
Guyschaos <guyschaos@gmail.com>
2020-03-14 13:48:56 +0800

    Add more logs...

--- root/aria2/script/delete.aria2.sh
+++ root/aria2/script/delete.aria2.sh
@@ -20,17 +20,20 @@ if [ $2 -eq 0 ]
 elif [ "$path" = "$filepath" ] && [ $2 -eq 1 ]
     then
         [ -e "$filepath".aria2 ] && rm -vf "$filepath".aria2
+        echo "move [$filepath] to COMPLETED;"
         mv "$filepath" /downloads/COMPLETED
         exit 0
 elif [ "$path" != "$filepath" ] && [ $2 -gt 1 ]
     then
         [ -e "$dirpath".aria2 ] && rm -vf "$dirpath".aria2
-        mv "$dirpath" /downloads/COMPLETED
+        echo "move [$path] to COMPLETED;"
+        mv "$path" /downloads/COMPLETED
         exit 0
 elif [ "$path" != "$filepath" ] && [ $2 -eq 1 ]
     then
         [ -e "$filepath".aria2 ] && rm -vf "$filepath".aria2
         # torrent task with folder but only 1 file.
+        echo "move [$path] to COMPLETED;"
         mv "$path" /downloads/COMPLETED
         exit 0
 fi
